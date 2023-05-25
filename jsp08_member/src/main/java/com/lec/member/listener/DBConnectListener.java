9d43905a2350ae03c0c1559ca9a735f51f08fcbf
[Github <[Github>
2023-05-25 22:34:00 +0900

    jsp08_member

---
+++ jsp08_member/src/main/java/com/lec/member/listener/DBConnectListener.java
@@ -0,0 +1,26 @@
+package com.lec.member.listener;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class DBConnectListener implements ServletContextListener{
+	public DBConnectListener() {}
+	
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		// web.xml context parameter 가져오기
+		ServletContext context = sce.getServletContext();
+		String drv = context.getInitParameter("driver");
+		try {
+			Class.forName(drv);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		}
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		// TODO Auto-generated method stub
+		ServletContextListener.super.contextDestroyed(sce);
+	}
+}
